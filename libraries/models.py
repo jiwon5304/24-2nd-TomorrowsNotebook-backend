c35b3cca04b4d95628464567510c0d432e995b1a
신우주 <newspace@sin-ujuui-MacBookPro.local>
2021-09-17 17:29:00 +0900

    socialloginview 수정, 테스트파일 수정

--- libraries/models.py
+++ libraries/models.py
@@ -5,22 +5,19 @@ from core.models import TimeStamp
 
 
 class Library(TimeStamp):
-    favorite     = models.BooleanField(default = False)
-    reading      = models.BooleanField(default = False)
-    current_page = models.IntegerField()
     user         = models.ForeignKey(User, on_delete = models.CASCADE)
     book         = models.ManyToManyField("books.Book", through = "LibraryBook")
     
     class Meta:
         db_table = "libraries"
 
-    def __str__(self):
-        return self.user
-
 class LibraryBook(models.Model):
-    book    = models.ForeignKey("books.Book", on_delete = models.CASCADE)
-    library = models.ForeignKey("Library", on_delete = models.CASCADE)
-    shelf   = models.ForeignKey("books.Shelf", on_delete = models.CASCADE)
+    favorite     = models.BooleanField(default = False)
+    reading      = models.BooleanField(default = False)
+    current_page = models.IntegerField(default = 0)
+    book         = models.ForeignKey("books.Book", on_delete = models.CASCADE)
+    library      = models.ForeignKey("Library", on_delete = models.CASCADE)
+    shelf        = models.ForeignKey("books.Shelf", on_delete = models.CASCADE)
 
     class Meta:
         db_table = "library_books"
