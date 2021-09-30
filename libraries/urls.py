722ad46b1d0e66ac5e3de0ac8680bd0f53d4458f
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-09-30 15:55:36 +0900

    Add : add books and shelflist function

--- libraries/urls.py
+++ libraries/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from libraries.views import ShelfDeleteView,LibraryListView
+from libraries.views import ShelfDeleteView, LibraryListView, ShelfListView
 
 urlpatterns = [
     path('/shelfdelete', ShelfDeleteView.as_view()),
     path('/<int:book_id>', LibraryListView.as_view()),
+    path('/shelflist', ShelfListView.as_view()),
 ]
--- libraries/views.py
+++ libraries/views.py
@@ -8,24 +8,7 @@ from users.decorators import login_decorator
 from books.models import Author, Book, Shelf
 from libraries.models import LibraryBook, Library
 
-class ShelfDeleteView(View):
-    @login_decorator
-    def post(self, request, **kwargs):
-        try:
-            data = json.loads(request.body)
-            shelf_id = data["shelf_id"]
 
-            if not shelf_id:
-                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=404)
-
-            user_library = Library.objects.get(user_id=request.user.id)
-            user_librarybooks = user_library.shelf_set.all().filter(id=shelf_id)
-            user_librarybooks.delete()
-            return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
-
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
-            
 class LibraryListView(View):
     def get(self, request, book_id):
         librarybook = LibraryBook.objects.filter(book_id = book_id)
@@ -46,4 +29,71 @@ class LibraryListView(View):
                 "user_nickname" : user_library.user.nickname,
                 "user_url" : user_library.user.profile_image_url
             })
+            
+class ShelfListView(View):
+    @login_decorator
+    def post(self, request, **kwargs):
+        try: 
+            data = json.loads(request.body)
+            shelf_id = data["shelf_id"]  
+            book_id = data["book_id"]      
+            user_library = Library.objects.get(user_id=request.user.id)
+
+            if not shelf_id:
+                return JsonResponse({"MESSAGE": "SHELF ERROR"}, status=404)
+
+            if not book_id:
+                return JsonResponse({"MESSAGE": "BOOK ERROR"}, status=404)
+            
+            if LibraryBook.objects.filter(book_id=book_id, shelf_id=shelf_id).exists():
+                    return JsonResponse({"MESSAGE": "ALREADY EXISTED BOOK IN SHELF"}, status=404)
+
+            LibraryBook.objects.create(
+                book_id = book_id,
+                library_id = user_library.id,
+                shelf_id = shelf_id,
+                favorite = False,
+                reading = False,
+                current_page = 0,
+            )
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
+
+    @login_decorator
+    def get(self, request):
+        user         = User.objects.get(id=request.user.id)    
+        user_library = Library.objects.get(user_id=request.user.id)   
+
+        totalshelves   = user_library.shelf_set.all()
+
+        shelves_list = [{
+            "shelf_id"  : shelf.id,
+            "shelf_name" : shelf.name
+            }for shelf in totalshelves ]
+
+        results = [{
+            "user_nickname" : user.nickname,
+            "user_image"    : user.profile_image_url,
+            "shelves_name"  : shelves_list
+        }]
         return JsonResponse({"results": results}, status=200)
+
+class ShelfDeleteView(View):
+    @login_decorator
+    def post(self, request, **kwargs):
+        try:
+            data = json.loads(request.body)
+            shelf_id = data["shelf_id"]
+
+            if not shelf_id:
+                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=404)
+
+            user_library = Library.objects.get(user_id=request.user.id)
+            user_librarybooks = user_library.shelf_set.all().filter(id=shelf_id)
+            user_librarybooks.delete()
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
+            
