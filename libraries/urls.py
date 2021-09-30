c5b33b412107f213deb1f2053b34631322eeb8c9
Jiwon <jiwon@Jiwonui-MacBookAir.local>
2021-09-30 15:49:21 +0900

    Add : delete file

--- libraries/urls.py
+++ libraries/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from libraries.views import ShelfDeleteView
+from libraries.views import ShelfDeleteView,LibraryListView
 
 urlpatterns = [
     path('/shelfdelete', ShelfDeleteView.as_view()),
+    path('/<int:book_id>', LibraryListView.as_view()),
 ]
