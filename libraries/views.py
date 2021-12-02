002d1dc43d1285e50f234a810ec9d819f7f50077
Jiwon <yesjiwon5304@gmail.com>
2021-12-03 01:33:31 +0900

    Add : refactoring - LibraryListView 중복제거 distinct() 적용 - ShelfListView 에러코드 수정 - ShelfDeleteView 에러코드 수정 - LibraryView 중복제거 distinct() 적용 및 에러코드 수정 - ShelfView 중복제거 distinct() 적용 및 에러코드 수정

--- libraries/views.py
+++ libraries/views.py
@@ -1,165 +1,178 @@
 import json
 
-from django.http import JsonResponse
-from django.views import View
+from django.http            import JsonResponse
+from django.views           import View
 
-from users.models import User
-from users.decorators import login_decorator
-from books.models import Author, Book, Shelf
-from libraries.models import LibraryBook, Library
+from users.models           import User
+from books.models           import Author, Book, Shelf, Publisher
+from libraries.models       import LibraryBook, Library
+from users.decorators       import login_decorator
 
 
+# 해당 도서를 서재에 담고 있는 모든 유저의 프로필을 반환.
+# 리팩토링 내용 : 중복 제거할 때 distinct() 를 사용하여 코드 간결화
 class LibraryListView(View):
     def get(self, request, book_id):
-        librarybook = LibraryBook.objects.filter(book_id = book_id)
+        librarybooks = LibraryBook.objects.filter(book_id=book_id)
+        libraries    = librarybooks.values_list('library_id', flat=True).distinct()
 
-        double_check_list = []
-        list = []
-        for id in librarybook:
-            if id.library_id not in double_check_list:
-                list.append(id.library_id)
-                double_check_list.append(id.library_id)
-        
-        results=[]
-        results.append({"total_library_count" : len(list)})
+        results = []
+        results.append({"total_libraries_count" : len(libraries)})
 
-        for user_id in list:
-            user_library = Library.objects.get(id=user_id)
+        for library in libraries:
+            users = Library.objects.get(id=library).user_id
+            user  = User.objects.get(id=users)
+
             results.append({
-                "user_nickname" : user_library.user.nickname,
-                "user_url" : user_library.user.profile_image_url
+                "user_nickname" : user.nickname,
+                "user_url"      : user.profile_image_url
             })
         return JsonResponse({"results": results}, status=200)
-            
+
+
+# 책 id & 책장 id를 받아 책장에 책 담기.
+# 리팩토링 내용 : 에러코드수정(잘못된요청-400)
 class ShelfListView(View):
     @login_decorator
     def post(self, request, **kwargs):
         try: 
-            data = json.loads(request.body)
-            shelf_id = data["shelf_id"]  
-            book_id = data["book_id"]      
+            data         = json.loads(request.body)
+            shelf_id     = data["shelf_id"]  
+            book_id      = data["book_id"]      
             user_library = Library.objects.get(user_id=request.user.id)
 
             if not shelf_id:
-                return JsonResponse({"MESSAGE": "SHELF ERROR"}, status=404)
+                return JsonResponse({"MESSAGE": "SHELF ERROR"}, status=400)
 
             if not book_id:
-                return JsonResponse({"MESSAGE": "BOOK ERROR"}, status=404)
+                return JsonResponse({"MESSAGE": "BOOK ERROR"}, status=400)
             
             if LibraryBook.objects.filter(book_id=book_id, shelf_id=shelf_id).exists():
-                    return JsonResponse({"MESSAGE": "ALREADY EXISTED BOOK IN SHELF"}, status=404)
+                    return JsonResponse({"MESSAGE": "ALREADY EXISTED BOOK IN SHELF"}, status=400)
 
             LibraryBook.objects.create(
-                book_id = book_id,
-                library_id = user_library.id,
-                shelf_id = shelf_id,
-                favorite = False,
-                reading = False,
+                book_id      = book_id,
+                library_id   = user_library.id,
+                shelf_id     = shelf_id,
+                favorite     = False,
+                reading      = False,
                 current_page = 0,
             )
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
 
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+    # 해당 유저의 책장 리스트 조회
     @login_decorator
     def get(self, request):
-        user         = User.objects.get(id=request.user.id)    
-        user_library = Library.objects.get(user_id=request.user.id)   
-        totalshelves   = user_library.shelf_set.all()
+            user          = User.objects.get(id=request.user.id)      
+            user_library  = Library.objects.get(user_id=request.user.id)   
+            totalshelves  = user_library.shelf_set.all()
 
-        shelves_list = [{
-            "shelf_id"  : shelf.id,
-            "shelf_name" : shelf.name
-            }for shelf in totalshelves ]
+            shelves_list = [{
+                "shelf_id"   : shelf.id,
+                "shelf_name" : shelf.name
+                }for shelf in totalshelves ]
 
-        results = [{
-            "user_nickname" : user.nickname,
-            "user_image"    : user.profile_image_url,
-            "shelves_name"  : shelves_list
-        }]
-        return JsonResponse({"results": results}, status=200)
+            results = [{
+                "user_nickname" : user.nickname,
+                "user_image"    : user.profile_image_url,
+                "shelves_name"  : shelves_list
+            }]
+            return JsonResponse({"results": results}, status=200)
 
+
+# 책장 삭제
+# 리팩토링 : 에러코드수정(잘못된요청-400)
 class ShelfDeleteView(View):
     @login_decorator
     def post(self, request, **kwargs):
         try:
-            data = json.loads(request.body)
+            data     = json.loads(request.body)
             shelf_id = data["shelf_id"]
 
             if not shelf_id:
-                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=404)
+                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=400)
 
-            user_library = Library.objects.get(user_id=request.user.id)
+            user_library      = Library.objects.get(user_id=request.user.id)
             user_librarybooks = user_library.shelf_set.all().filter(id=shelf_id)
             user_librarybooks.delete()
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
 
         except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
 
 class LibraryView(View):
+    # 책장 추가
+    # 리팩토링 : 에러코드수정(잘못된요청-400)
     @login_decorator
     def post(self, request, **kwargs):
         try:
-            data = json.loads(request.body)
-            shelf_name = data["shelf_name"]
-            user_library = Library.objects.get(user_id=request.user.id)
+            data          = json.loads(request.body)
+            shelf_name    = data["shelf_name"]
+            user_library  = Library.objects.get(user_id=request.user.id)
             
             if not shelf_name:
-                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=404)
+                return JsonResponse({"MESSAGE": "INPUT ERROR"}, status=400)
 
             if Shelf.objects.filter(library_id=user_library.id, name=shelf_name).exists():
-                return JsonResponse({"MESSAGE": "ALREADY EXISTED SHELF"}, status=404)
+                return JsonResponse({"MESSAGE": "ALREADY EXISTED SHELF"}, status=400)
 
             Shelf.objects.create(
-                name = data["shelf_name"],
+                name       = data["shelf_name"],
                 library_id = user_library.id
             )
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
         except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=404)
-            
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+    
+    # 전체 도서 상세 조회
+    # 리팩토링 : 중복 제거할 때 distinct() 를 사용하여 코드 간결화, 에러코드수정(잘못된요청-400)
     @login_decorator
     def get(self, request):
-        try:
-            user         = User.objects.get(id=request.user.id)           
+        try:                
+            user         = User.objects.get(id=request.user.id)                    
             user_library = Library.objects.get(user_id=request.user.id)
             totalbooks   = user_library.librarybook_set.all()
+            books = totalbooks.values_list('book_id', flat=True).distinct()
 
             totalbook_list = []
+
+            for book in books:
+                favorite     = LibraryBook.objects.filter(book_id=book).values_list('book_id', flat=True).distinct().values()[0]['favorite']
+                book         = Book.objects.get(id=book)
+                bookauthor   = book.bookauthor_set.all().values()[0]['author_id']
+                author       = Author.objects.get(id=bookauthor).name
+                publisher    = Publisher.objects.get(id=book.publisher_id).name
+
+                totalbook_list.append({    
+                        "book_id"           : book.id,
+                        "book_name"         : book.title,
+                        "book_image"        : book.image_url,
+                        "book_author"       : author,
+                        "book_current_page" : book.page,      
+                        "book_publish_date" : book.publish_date,
+                        "book_publisher"    : publisher,        
+                        "favorite"          : favorite
+                    })
+            
             results = [{
-                "user_nickname" : user.nickname,
-                "user_image"    : user.profile_image_url,
+                "user_nickname"   : user.nickname,
+                "user_image"      : user.profile_image_url,
                 "user_totalbooks" : totalbook_list,
             }]
-
-            double_check_list = []
-
-            for books in totalbooks :           
-                bookauthor = books.book.bookauthor_set.all().values()
-                book_author = Author.objects.get(id=bookauthor[0]['author_id']).name
-            
-                if books.book_id not in double_check_list:
-                    totalbook_list.append({    
-                        "book_id"           : books.book_id,
-                        "book_name"         : books.book.title,
-                        "book_image"        : books.book.image_url,
-                        "book_author"       : book_author,
-                        "book_current_page" : books.current_page,      
-                        "book_publish_date" : books.book.publish_date,
-                        "book_publisher"    : books.book.publisher.name,        
-                        "favorite"          : books.favorite
-                    })
-                    double_check_list.append(books.book_id)
-            
             return JsonResponse({"results": results}, status=200)
             
         except KeyError:
-            return JsonResponse({"MESSAGE": "KEY ERROR"}, status=404)
+            return JsonResponse({"MESSAGE": "KEY ERROR"}, status=400)
 
         except User.DoesNotExist:
-            return JsonResponse({"MESSAGE": "User Does Not Exists"}, status=404)
+            return JsonResponse({"MESSAGE": "User Does Not Exists"}, status=400)
 
+
+# 책장 별 도서 조회
+# 리팩토링 : 중복 제거할 때 distinct() 를 사용하여 코드 간결화, 에러코드수정(잘못된요청-400)
 class ShelfView(View):
     @login_decorator
     def get(self, request):
@@ -168,56 +181,57 @@ class ShelfView(View):
             user_library = Library.objects.get(user_id=request.user.id)
             shelves      = Shelf.objects.filter(library_id=user_library.id)
             librarybooks = LibraryBook.objects.select_related('book','shelf').filter(library_id=user_library.id)
+            librarybook  = librarybooks.values_list('book_id', flat=True).distinct()
             
-            shelves_list = []
-            results = [{
-                "user_nickname" : user.nickname,
-                "user_image"    : user.profile_image_url,
-                "user_shelves"  : shelves_list,
-            }]
+            shelves_list       = []
+            readingbooks_list  = []
+            favoritebooks_list = []
 
-            readingbooks_list = []
             shelves_list.append({
                         "shelf_name":  "읽고있는 책",
                         "book_image" : readingbooks_list
                     })
-
-            double_check_list =[]
-            for books in librarybooks :
-                if books.reading == True and books.book.image_url not in double_check_list:
-                    readingbooks_list.append(books.book.image_url)
-                    double_check_list.append(books.book.image_url)
-
-            favoritebooks_list = []
             shelves_list.append({
                 "shelf_name":  "My Favorite",
                 "book_image" : favoritebooks_list
             })
 
-            double_check_list =[]
-            for books in librarybooks:
-                if books.favorite == True and books.book.image_url not in double_check_list:
-                    favoritebooks_list.append(books.book.image_url)
-                    double_check_list.append(books.book.image_url)
+            for books in librarybook:
+                book = LibraryBook.objects.filter(book_id=books).values_list('book_id', flat=True).distinct()
+
+                if book.values()[0]['reading'] == True and book.values()[0]['favorite'] == True:
+                    readingbooks_list.append(Book.objects.get(id=books).image_url)
+                    favoritebooks_list.append(Book.objects.get(id=books).image_url)
+                
+                elif book.values()[0]['reading'] == True:
+                    readingbooks_list.append(Book.objects.get(id=books).image_url)
+                
+                elif book.values()[0]['favorite'] == True:
+                    favoritebooks_list.append(Book.objects.get(id=books).image_url)
 
             for shelf in shelves:
                 book_list = []
                 shelves_list.append({
-                    "shelf_id" : shelf.id,
-                    "shelf_name":  shelf.name,
+                    "shelf_id"   : shelf.id,
+                    "shelf_name" :  shelf.name,
                     "book_image" : book_list
                 })
-            
-                library = librarybooks.filter(shelf_id = shelf.id)
-                for books in library:
+
+                for books in librarybooks.filter(shelf_id=shelf.id):
                     book_list.append(books.book.image_url)
+
+            results = [{
+                "user_nickname" : user.nickname,
+                "user_image"    : user.profile_image_url,
+                "user_shelves"  : shelves_list,
+            }]
             return JsonResponse({"results": results}, status=200)
             
         except KeyError:
-            return JsonResponse({"MESSAGE": "KEY ERROR"}, status=404)
+            return JsonResponse({"MESSAGE": "KEY ERROR"}, status=400)
 
         except User.DoesNotExist:
-            return JsonResponse({"MESSAGE": "User Does Not Exists"}, status=404)
+            return JsonResponse({"MESSAGE": "User Does Not Exists"}, status=400)
 
 class ViewerView(View):
     @login_decorator
@@ -226,13 +240,13 @@ class ViewerView(View):
             library_book = LibraryBook.objects.select_related('book', 'library', 'book__book_info').filter(library__user = request.user, book__id=book_id).prefetch_related('book__author')
 
             result = [{
-                'book_id' : library.book.id,
-                'title' : library.book.title,
-                'author' : [author.name for author in library.book.author.all()],
-                'book_info' : library.book.book_info.contents,
-                'image_url' : library.book.image_url,
+                'book_id'      : library.book.id,
+                'title'        : library.book.title,
+                'author'       : [author.name for author in library.book.author.all()],
+                'book_info'    : library.book.book_info.contents,
+                'image_url'    : library.book.image_url,
                 'current_page' : library.current_page,
-                's3_url' : library.book.book_info.text
+                's3_url'       : library.book.book_info.text
             }for library in library_book]
 
             return JsonResponse({'RESULT' : result}, status = 200)
